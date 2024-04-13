25f6d37efffaa03a702c8f75dae96cc8b262fa2e
Shalini Kammalam <115170181+ShaliniKammalam@users.noreply.github.com>
2024-04-13 18:15:53 +0530

    Add files via upload

---
+++ src/main/java/com/thymeleafspringbootapplication/service/EmployeeServiceImpl.java
@@ -0,0 +1,44 @@
+package com.thymeleafspringbootapplication.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thymeleafspringbootapplication.model.Employee;
+import com.thymeleafspringbootapplication.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(String userid) {
+        Optional<Employee> optional = employeeRepository.findById(userid);
+        Employee employee;
+        if (optional.isPresent()) {
+            employee =  optional.get();
+        } else {
+            throw new RuntimeException(" Employee not found for userid :: " + userid);
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(String userid) {
+        this.employeeRepository.deleteById(userid);
+    }
+}
